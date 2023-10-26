cf551cca6584492822b0e67949e63de427eebd7b
kdzuo <kdz1062735832@gmail.com>
2023-10-25 19:00:26 -0700

    complement all resources of post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,38 @@
 class PostsController < ApplicationController
   http_basic_authenticate_with name:"kk", password:"kk", only: [:destroy]
+  def show
+    @user =  User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
   def create
     @user = User.find(params[:user_id])
-    @post = @user.posts.create(post_params)
-    redirect_to @user
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+
+  def edit
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to user_post_path(@user,@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @user = User.find(params[:user_id]);
     @post = @user.posts.find(params[:id])
@@ -13,6 +41,6 @@ class PostsController < ApplicationController
   end
   private
     def post_params
-      params.require(:post).permit(:poster, :content, :status)
+      params.require(:post).permit(:poster, :content, :status, :title)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_235843) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_005931) do
   create_table "posts", force: :cascade do |t|
     t.string "poster"
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_235843) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
